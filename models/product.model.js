29f48c2acb0e6f703f9bc3a611741a25528a23ef
Mahdi Vajdi <mahdivajdii@gmail.com>
2023-02-08 23:19:25 +0330

    Completed product model schema

--- models/product.model.js
+++ models/product.model.js
@@ -2,9 +2,62 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  name: String,
-  detail: String,
-  quantity: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countSchema: {
+    type: Number,
+    required: 0,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 exports.Product = mongoose.model("Product", productSchema);
